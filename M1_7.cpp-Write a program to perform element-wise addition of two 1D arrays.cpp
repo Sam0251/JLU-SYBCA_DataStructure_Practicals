f770f37da9e7b0c7ecd3b4dae0699abaae2ae2c5
Sam0251 <jlu09454@student.jlu.edu.in>
2025-08-22 11:38:09 +0530

    Add files via upload

---
+++ M1_7.cpp-Write a program to perform element-wise addition of two 1D arrays.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr1[5], arr2[5], sum[5];
+    cout << "Enter 5 elements for the first array:" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "arr1[" << i << "]: ";
+        cin >> arr1[i];
+    }
+    cout << "\nEnter 5 elements for the second array:" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "arr2[" << i << "]: ";
+        cin >> arr2[i];
+    }
+
+    for (int i = 0; i < 5; i++) {
+        sum[i] = arr1[i] + arr2[i];
+    }
+    cout << "\nElement-wise sum of the two arrays:" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "sum[" << i << "] = " << sum[i] << endl;
+    }
+     return 0;
+}
+
+
