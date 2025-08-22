1e5b416a177f02ba8616d240719b6efaf2eeca4d
Sam0251 <jlu09454@student.jlu.edu.in>
2025-08-22 11:15:09 +0530

    Add files via upload

---
+++ M1_5.cpp-Write a program to reverse the elements of a 1D array.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+    cout << "Enter 10 elements of the array:" << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << "Element " << i + 1 << ": ";
+        cin >> arr[i];
+    }
+    for (int i = 0; i < 5; i++) {
+        int temp = arr[i];
+        arr[i] = arr[9 - i];
+        arr[9 - i] = temp;
+    }
+
+
+    cout << "Reversed array:" << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+
+
+
+
