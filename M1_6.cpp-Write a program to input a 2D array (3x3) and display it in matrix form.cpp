02299bb127815e60dc2b1f7f63135c9f63456c16
Sam0251 <jlu09454@student.jlu.edu.in>
2025-08-22 11:18:57 +0530

    Add files via upload

---
+++ M1_6.cpp-Write a program to input a 2D array (3x3) and display it in matrix form.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int matrix[3][3];
+
+
+    cout << "Enter elements for a 3x3 matrix:" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << "Element [" << i << "][" << j << "]: ";
+            cin >> matrix[i][j];
+        }
+    }
+	cout << "Matrix form:" << endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << matrix[i][j] << "\t";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+
