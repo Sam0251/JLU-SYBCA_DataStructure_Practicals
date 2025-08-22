2011698860d717e4702ebb7d7cb382cf362c7541
Sam0251 <jlu09454@student.jlu.edu.in>
2025-08-22 10:54:56 +0530

    Add files via upload

---
+++ Write a program to input 10 numbers in an array and display them.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int numbers[10];
+    cout << "Enter 10 integers:" << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << "Element " << i + 1 << ": ";
+        cin >> numbers[i];
+    }
+
+    cout << "Enter a number" << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << "Element at index " << i << " = " << numbers[i] << endl;
+    }
+
+    return 0;
+}
+
+
