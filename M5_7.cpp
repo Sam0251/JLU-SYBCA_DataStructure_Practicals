184800e8747894887e2a0e102de5adb5e401b9e8
Sam0251 <jlu09454@student.jlu.edu.in>
2025-11-30 22:03:51 +0530

    Add files via upload

---
+++ M5_7.cpp
@@ -0,0 +1,27 @@
+//Write a program to sort an array using bubble sort.
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int a[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    for (int i = 0; i < n - 1; i++)
+        for (int j = 0; j < n - i - 1; j++)
+            if (a[j] > a[j + 1]) {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+
+    printf("Sorted Array: ");
+    for (int i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
+
